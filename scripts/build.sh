9f2b61c087b7161d99266353e73bb1b0368b9c03
Max Claus Nunes <maxcnunes@gmail.com>
2024-12-30 14:29:47 -0300

    Move scripts to a directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 # Script used by https://render.com/ on deployments.
-echo "Starting build"
-pwd
-ls -la
-node --version
-npm --version
+
+echo "Building client"
 (
   cd app
   npm ci
   npm run build
 )
-pwd
-ls -la
-ls -la app
-ls -la app/dist
+
+echo "Building server"
 cargo build --release
